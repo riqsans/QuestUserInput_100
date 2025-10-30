8e7864826975eedf247721ec24172a97b755284a
riqsans <thrq88@gmail.com>
2025-10-30 13:03:37 +0700

    menambahkan divider untuk memisahkan form dan tombol submit

--- app/src/main/java/com/example/userlayout/TeksEdit.kt
+++ app/src/main/java/com/example/userlayout/TeksEdit.kt
@@ -88,4 +88,12 @@ fun FormDataDiri(modifier: Modifier) {
                 }
             )
         }
-        }
+        Divider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )
+            ),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
