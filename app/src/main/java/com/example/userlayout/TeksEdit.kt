dba37236f6c0811db27e486c92de45caaf7af421
riqsans <thrq88@gmail.com>
2025-10-30 12:59:24 +0700

    membuat gender dan loop radio button

--- app/src/main/java/com/example/userlayout/TeksEdit.kt
+++ app/src/main/java/com/example/userlayout/TeksEdit.kt
@@ -63,3 +63,19 @@ fun FormDataDiri(modifier: Modifier) {
                 textNama = it
             }
         )
+        Row {
+            gender.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
+                }
+            }
+            }
