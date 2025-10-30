61dfc286335d528583f65ae3fda09fe59a4d082b
riqsans <thrq88@gmail.com>
2025-10-30 13:05:12 +0700

    mememperbaiki error

--- app/src/main/java/com/example/userlayout/TeksEdit.kt
+++ app/src/main/java/com/example/userlayout/TeksEdit.kt
@@ -106,4 +106,7 @@ fun FormDataDiri(modifier: Modifier) {
                 jenis = textJK
                 alamat = textAlamat
             }
-        )
+        ) {
+            Text(stringResource(R.string.submit))
+        }
+
